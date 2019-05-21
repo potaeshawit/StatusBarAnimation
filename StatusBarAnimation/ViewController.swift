9b52821d926ef88237225acc16d49350c3cd2d3e
Taeshawit Suwanwigo <s.taeshawit@gmail.com>
2019-05-22 00:01:13 +0700

    initial commit

---
+++ StatusBarAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StatusBarAnimation
+//
+//  Created by Taeshawit Suwanwigo on 21/5/2562 BE.
+//  Copyright © 2562 Taeshawit Suwanwigo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
