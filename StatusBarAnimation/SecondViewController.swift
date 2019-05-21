d5d41b47a88127e1fc266c9d109c373218716b6a
Taeshawit Suwanwigo <s.taeshawit@gmail.com>
2019-05-22 00:27:58 +0700

    second view controller logic

---
+++ StatusBarAnimation/SecondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  StatusBarAnimation
+//
+//  Created by Taeshawit Suwanwigo on 22/5/2562 BE.
+//  Copyright © 2562 Taeshawit Suwanwigo. All rights reserved.
+//
+
+import UIKit
+
+final class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction private func dismissButtonDidTouch(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+}
